f02054312b27de7a7c797de49d1b886f7be54d43
abdulrahmansamy <abdul.m.samy@gmail.com>
2024-08-03 17:12:36 +0300

    Updating the Cloud Shell Script

---
+++ 01-Google/01-DevOps/bash_scripts/RollbackScript.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Roll back script
+
+ZONE=us-central1-a
+
+gcloud container clusters delete hello-cluster  --zone $ZONE --async
+
+gcloud container clusters list --format="csv(name,status)"
+
+SEC=0
+while gcloud container clusters list --format="csv(name,status)" | grep -q STOPPING
+do
+    # Calculate elapsed time
+    minutes=$((SEC / 60))
+    seconds=$((SEC % 60))
+
+    # echo -ne "Cluster still in Stopping State: $SEC seconds\r"
+    printf "\rCluster still in Stopping State - Elapsed time: %02d minutes and %02d seconds" $minutes $seconds
+    let SEC=SEC+2
+    sleep 2 
+done
+
+echo
+gcloud container clusters list --format="csv(name,status)"
+
+
+gcloud artifacts repositories delete my-repository  --location=$REGION 
+
+
+gcloud source repos delete sample-app
+
+rm -fr ~/sample-app
+
+gcloud builds triggers delete sample-app-prod-deploy
+
+gcloud builds triggers delete sample-app-dev-deploy
